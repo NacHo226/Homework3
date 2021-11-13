abdc0d1afb782c909e42d850e50cde6f642413fb
NacHo226 <83178863+NacHo226@users.noreply.github.com>
2021-11-13 17:04:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Task1.ReportGenerator.Classes;
+using Task1.User.Classes;
+using Task1.User.Classes.Employee;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] users = {"Employee",  "Candidate"};
+
+            Random rnd = new Random();
+            int maxValueOfUsers = rnd.Next(30);
+
+            var employees = new List<Employee>();
+            var candidates = new List<Candidate>();
+            var userFactory = new UserFactory();
+
+            for (int i = 0; i < maxValueOfUsers; i++)
+            {
+                userFactory.GenerateUser(/*Convert.ToString((Users)*/ users[rnd.Next(2)]/*)*/, employees, candidates);
+            }
+
+            foreach (var user in employees)
+            {
+                user.Display();
+                Console.WriteLine();
+            }
+
+            foreach (var user in candidates)
+            {
+                user.Display();
+                Console.WriteLine();
+            }
+
+            var erg = new EmployeeReportGenerator();
+            erg.SortEmployee(employees);
+
+            var crg = new CandidateReportGenerator();
+            crg.SortCandidate(candidates);
+        }
+    }
+}
---
+++ User/Classes/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Task1.User.Interfaces;
+
+namespace Task1.User.Classes
+{
+    public class User : IDisplayable
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+        public string JobDescription { get; set; }
+        public decimal JobSalary { get; set; }
+        public string FullName => ($"{FirstName} {LastName}");
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"Hello, I'm {FullName}.");
+        }
+    }
+}
